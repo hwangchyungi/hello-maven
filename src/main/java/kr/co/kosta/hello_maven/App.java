fb78180baeb8ace1642f68cd94d512c7e7640305
kosta <kosta@kosta-PC>
2016-07-01 10:12:19 +0900

    첫수정

--- src/main/java/kr/co/kosta/hello_maven/App.java
+++ src/main/java/kr/co/kosta/hello_maven/App.java
@@ -4,10 +4,12 @@ package kr.co.kosta.hello_maven;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+	}
 }
